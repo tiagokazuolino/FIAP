1c25b3dc2cfafcbb811cb0235bf1fffd12eb9827
Tiago Lino <tiagokazuolino@hotmail.com>
2020-04-13 19:48:09 -0300

    Implements Pilha int Methods

---
+++ CAP/Pilha/PilhaEncodeDecode/src/br/sp/edu/fiap/interfaces/IPilha.java
@@ -0,0 +1,9 @@
+package br.sp.edu.fiap.interfaces;
+
+public interface IPilha {
+	void init();
+
+	boolean isFull();
+
+	boolean isEmpty();
+}
---
+++ CAP/Pilha/PilhaEncodeDecode/src/br/sp/edu/fiap/interfaces/IPilhaInt.java
@@ -0,0 +1,9 @@
+package br.sp.edu.fiap.interfaces;
+
+public interface IPilhaInt extends IPilha {
+	void push(int valor);
+
+	int pop();
+
+	int top();
+}
