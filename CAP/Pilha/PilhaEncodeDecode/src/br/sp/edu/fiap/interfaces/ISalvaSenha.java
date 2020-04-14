1da2ef327ccfa0038f4f71c2d73ccbe8cf2c2be3
Tiago Lino <tiagokazuolino@hotmail.com>
2020-04-13 23:34:56 -0300

    fix: Remove principal

--- CAP/Pilha/PilhaEncodeDecode/src/br/sp/edu/fiap/interfaces/ISalvaSenha.java
+++
@@ -1,5 +0,0 @@
-package br.sp.edu.fiap.interfaces;
-
-public interface ISalvaSenha {
-	String inverter(String palavra);
-}
--- CAP/Pilha/PilhaEncodeDecode/src/br/sp/edu/fiap/main/Principal.java
+++
@@ -1,7 +0,0 @@
-package br.sp.edu.fiap.main;
-
-public class Principal {
-	public static void main(String[] args) {
-
-	}
-}
