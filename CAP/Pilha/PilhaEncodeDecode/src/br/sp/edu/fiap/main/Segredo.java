75c7703aab3d8962cdb99063a5650373f3866719
Tiago Lino <tiagokazuolino@hotmail.com>
2020-04-14 19:27:45 -0300

    Alteração para modo console

--- CAP/Pilha/PilhaEncodeDecode/src/br/sp/edu/fiap/main/Segredo.java
+++ CAP/Pilha/PilhaEncodeDecode/src/br/sp/edu/fiap/main/Segredo.java
@@ -7,37 +7,31 @@ import br.sp.edu.fiap.variables.PilhaChar;
 public class Segredo implements ISegredo {
 	static char[] original = new char[] { 'a', 's', 'r', 'e', 'i', 'o', 'u' };
 	static char[] troca = new char[] { '@', '$', '*', '&', '!', '(', '+' };
-
-	@Override
 	public String encode(String valor) {
 		String transform = valor.replaceAll("[#]", "\n#\n");
 		transform = transform.replaceAll("[_]", "\n_\n");
 		String[] palavras = transform.split("[\\n]");
 		String resultado = "";
-
 		for (int i = 0; i < palavras.length; i++) {
 			if (palavras[i] != "\n") {
 				resultado += inverter(palavras[i]);
 			}
-		}
-		char[] frase = resultado.toCharArray();
+		}		
+		char[] frase = resultado.toCharArray();		
 		for (int i = 0; i < frase.length; i++) {
 			for (int j = 0; j < original.length; j++) {
 				if (frase[i] == original[j]) {
 					frase[i] = troca[j];
 				}
 			}
-		}
+		}		
 		return new String(frase);
 	}
-
-	@Override
 	public String decode(String valor) {
 		String transform = valor.replaceAll("[#]", "\n#\n");
 		transform = transform.replaceAll("[_]", "\n_\n");
 		String[] palavras = transform.split("[\\n]");
 		String resultado = "";
-
 		for (int i = 0; i < palavras.length; i++) {
 			if (palavras[i] != "\n") {
 				resultado += inverter(palavras[i]);
@@ -53,20 +47,17 @@ public class Segredo implements ISegredo {
 		}
 		return new String(frase);
 	}
-
-	@Override
 	public String inverter(String palavra) {
 		char[] p = palavra.toCharArray();
 		IPilhaChar pilha = new PilhaChar(p.length);
 		pilha.init();
-		
 		for (int i = 0; i < p.length; i++) {
-			if(!pilha.isFull()) {
+			if (!pilha.isFull()) {
 				pilha.push(p[i]);
 			}
 		}
 		for (int i = 0; i < p.length; i++) {
-			if(!pilha.isEmpty()) {
+			if (!pilha.isEmpty()) {
 				p[i] = pilha.pop();
 			}
 		}
